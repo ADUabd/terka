06bdaa7503a93603ded11524f9339ce4d84d3754
ADUabd <97505497+ADUabd@users.noreply.github.com>
2022-01-13 11:41:13 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/chocolate2/MainActivity.java
+++ app/src/main/java/com/example/chocolate2/MainActivity.java
@@ -5,7 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+yhtgrf
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
